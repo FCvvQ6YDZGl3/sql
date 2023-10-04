306b87c66fe9ad891a8ac0f2cf2358ca75e26b96
Попов Петр <popov.petr.viktorovich@gmail.com>
2023-10-04 19:13:47 +0700

    Добавил в проект Factory продукцию и характеристики в EAV модели. наполнил тестовыми данными.

--- Factory/Spec/create_spc_product_values.sql
+++ Factory/Spec/create_spc_product_values.sql
@@ -1,11 +1,27 @@
---ѕока что нету таблицы продутов, но позже можно сослатьс€ на любой справочник.
+--ѕока что нету таблицы продуктов, но позже можно сослатьс€ на любой справочник.
 CREATE TABLE fct.spec_product
 (
 	sppr_id int identity not null,
 	sppr_spc_id int not null,
 	sppr_product_id int not null,
-	sppr_value varchar not null,
+	sppr_value varchar(1000) not null,
 	constraint fk_sppr_spc_id foreign key(sppr_spc_id) references fct.spec(spc_id),
 	constraint uq_sppr_spc_id_product_id unique (sppr_spc_id, sppr_product_id)
 )
-select * from fct.spec
+go
+
+alter table fct.spec_product
+	add sppr_prd_code char(100) not null;
+alter table fct.spec_product
+	drop column sppr_product_id
+alter table fct.spec_product
+	add constraint pk_sppr_sppr_id primary key (sppr_id)
+
+alter table fct.spec_product drop constraint uq_sppr_spc_id_product_id
+
+alter table fct.spec_product add constraint uq_sppr_spc_id_prd_code unique (sppr_spc_id, sppr_prd_code)
+go
+
+alter table fct.spec_product
+	add sppr_value varchar(1000)
+go
--- Factory/Spec/create_spec.sql
+++ Factory/Spec/create_spec.sql
@@ -9,13 +9,36 @@ CREATE TABLE fct.spec
 	constraint uq_spc_soc_name unique (spc_name),
 	constraint fk_spc_sql_type_id foreign key (spc_sql_type_id) references fct.spec_sql_type(sst_id)
 );
-IF (OBJECT_ID(N'fct.spec') IS NOT NULL)
-BEGIN
-	truncate table fct.spec;
-	insert into fct.spec
+select * from fct.spec_sql_type
+select * from fct.spec
+
+drop table if exists #fct_spec;
+select *
+into #fct_spec
+from
+(values
+('Длина', 1)
+,('Ширина', 1)
+,('Высота', 1)
+,('Вес', 1)
+,('Цвет', 2)) as s(spc_name, spc_sql_type_id)
+
+merge
+	fct.spec as trg
+using
+	#fct_spec as src
+on src.spc_name = trg.spc_name
+when not matched
+	then insert
+	(
+		spc_name,
+		spc_sql_type_id
+	)
 	values
-	('Длина', 1)
-	,('Ширина', 1)
-	,('Высота', 1)
-	,('Вес',1)
-END;
+	(
+		src.spc_name,
+		src.spc_sql_type_id
+	)
+when matched
+	then update set
+		trg.spc_sql_type_id = src.spc_sql_type_id;
